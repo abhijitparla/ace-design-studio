b85a10b5d33f94fd31bef80533d544d2c118fe46
Abhijit Parlapalli <abhijit.parla01@gmail.com>
2024-03-15 07:45:44 +0530

    loader changes

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const loading = () => {
   return (
-    <div>
-      loading...
+    <div className='b h-screen flex justify-center'>
+      <h1 className='my-auto'>loading...</h1>
     </div>
   )
 }
